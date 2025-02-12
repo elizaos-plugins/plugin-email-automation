c9f14ab40bc9f70c3046b92098c858435aef6c4e
Carlos V <carlosv.provoos@gmail.com>
2025-02-12 16:45:58 -0500

    chore: update plugin dist

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+declare const emailAutomationPlugin: {
+    name: string;
+    description: string;
+    services: any[];
+    clients: any[];
+    evaluators: any[];
+    providers: any[];
+};
+
+export { emailAutomationPlugin as default };
